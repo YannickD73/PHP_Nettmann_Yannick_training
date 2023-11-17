3e81c33ff53a09535c4dabb7d68fba275c5fad72
Yannick Deschaseaux <Commande@gmx.info>
2023-11-17 23:21:29 +0100

    test erste skript PHP

---
+++ Hallo World/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <Title>Hallo Welt!</Title>
+    </head>
+<body>
+<h1>Erstes PHP-Skript</h1>
+
+<?php
+
+echo "Hallo <br> Welt!";
+
+
+?>
+</body>
+
+</htnml>
